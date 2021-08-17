b6ddc8c190064b27ebe9b61fc4699faaa26455f2
Ana Rapp <zmraxton@gmail.com>
2021-08-17 08:22:15 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const pug = require('pug');
+const routes = require('./routes/routes');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Heaers", "Origin, X-Request-With, Content-Type, Accept");
+    next();
+});
+
+let urlEncodedParser = express.urlencoded({
+    extended: false
+});
+
+app.get('/login', routes.login);
+app.get('/', routes.api);
+app.get('/create', routes.create)
+app.post('/create', urlEncodedParser, routes.createAccount);
+app.get('/edit', routes.edit)
+app.post('/edit', urlEncodedParser, routes.editAccount);
+app.post('/delete', urlEncodedParser, routes.deleteAccount);
+
+app.listen(3000);
