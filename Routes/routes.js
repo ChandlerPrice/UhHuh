70fd01ec969b109ebbb1ba9a35ab872265a8ba28
Spencer <thebuff2001@gmail.com>
2021-08-17 08:27:10 -0600

    delete one routes started
    basic of routes done

---
+++ Routes/routes.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/data', 
+{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+let mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error'));
+mdb.once('open', callback => {});
+
